bb41237ddaffac3a6c32b713a5acfbc81db9108f
Abdelrahman-Security159 <abdelrahman.sec9@gmail.com>
2021-12-14 22:58:46 +0000

    finish some pages

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns  = [
-    path('', views.index),
+    path('', views.index, name="index"),
     path('posts', views.posts),
+    path('explore', views.explore, name="explore"),
     path('path/<str:slug>', views.single_post)
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,12 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "index.html")
-    
 
 def posts(request):
-    pass
+    return render(request, "post.html")
+
+def explore(request):
+    return render(request, "explore.html")
 
 def single_post(request):
     pass
