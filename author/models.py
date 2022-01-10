55bbbaf3cdd57b641c96a12b66e0e7677ebc0564
Abdelrahman-Security159 <abdelrahman.sec9@gmail.com>
2022-01-10 21:55:43 +0000

    Add Forms

--- author/models.py
+++ author/models.py
@@ -5,6 +5,8 @@ class Author(models.Model):
     user_name = models.CharField(max_length=20)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=35)
+    password = models.CharField(max_length=16)
     age = models.IntegerField()
     
     def __str__(self):
--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ urlpatterns  = [
     path('', views.index, name="index"),
     path('posts/<int:id>', views.posts, name="posts"),
     path('explore', views.explore, name="explore"),
-    path('path/<str:slug>', views.single_post)
+    path('path/<str:slug>', views.single_post),
 ]
