fd80178557063d489a99c3cfe3a8ed167aef4047
Abdelrahman-Security159 <abdelrahman.sec9@gmail.com>
2022-01-12 17:54:25 +0000

    Config the forms

--- author/views.py
+++ author/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
+from .forms import TestForm
 
 
 def register(request):
     return render(request, "register.html")
 
 def login(request):
-    return render(request, "login.html")
+    if request.method == "POST":
+        form = TestForm(request.POST)
+        
+        if form.is_valid():
+            return render(request, "done.html")
+    else:
+        form = TestForm()
+    return render(request, "login.html", {"form":form})
+
+def test(request):
+    if request.method == "POST":
+        form = TestForm(request.POST)
+        
+        if form.is_valid():
+            print(form.cleaned_data)
+            return render(request, "done.html")
+    else:
+        form = TestForm()
+    return render(request, "test.html", {"form":form})
